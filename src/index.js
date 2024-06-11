e469acd6dbb1c0e44e868b285a78e932d9026338
Kanesho1924 <avande764@gmail.com>
2024-06-11 10:11:12 +0200

    Fix 404 GCloud

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const customerRoutes = require('./routes/customers.js');
-const app = express()
-const port = 8080;
+const app = express();
+const port = 8081;
 
 app.use(bodyParser.json());
 app.use('/customers', customerRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Bienvenue');
+});
 
 app.listen(port, () => {
     console.log(`L'API Clients est exécutée à partir du port ${port}`);

a69d6c0c29b82ad40f49913b7388ecf9edafeb5f
Kanesho1924 <avande764@gmail.com>
2024-06-21 13:01:59 +0200

    Feature : Ajout Swagger + Test GCloud region change

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
 require('dotenv').config();
 const customerRoutes = require('./routes/customers.js');
 const app = express();
-const port = 8080;
+const port = 8081;
 
+const swaggerDocument = JSON.parse(fs.readFileSync('./src/swagger.json', 'utf8'));
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/customers', customerRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
     res.send('Bienvenue sur l\'API Clients');
 });
-
 
 app.listen(port, () => {
     console.log(`L'API Clients est exécutée à partir du port ${port}`);
